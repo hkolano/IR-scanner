b9baf480268c9f9323897ba0d59ad86c47f96c7b
hkolano <hannah.kolano@students.olin.edu>
2017-09-18 13:08:07 -0400

    scans 2d book accurately enough

--- plotscan.py
+++ plotscan.py
@@ -9,38 +9,57 @@ matplotlib tutorial: https://matplotlib.org/mpl_toolkits/mplot3d/tutorial.html
 
 '''import libraries'''
 from serial import Serial, SerialException
-cxn = Serial('/dev/ttyACM1', baudrate=9600)
+cxn = Serial('/dev/ttyACM0', baudrate=9600)
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
+from numpy.polynomial import Polynomial as P
+import math as m
 
-xvalues = [1, 2, 3]
-yvalues = [1, 2, 3]
+xvalues = []
+yvalues = []
 
 
 '''get data from arduino'''
 cmd_id = int(input("Select '1' to begin"))
-if cmd_id ==1:
+if cmd_id == 1:
     cxn.write([int(cmd_id)])
+else:
+    print("That's not the number 1. Learn to read.")
 
-while(True):
+i = 0
+while i <= 90:
         while cxn.inWaiting() < 1:
             pass
         result = cxn.readline()
         try:
             result = result.decode('UTF-8')
-            new_angle, hypotenuse_raw = result.split(' ')
-            print("angle", new_angle)
-            print("reading", hypotenuse_raw)
+            new_angle, read1, read2, read3, read4, read5 = result.split(' ')
+            hypotenuse_raw = (int(read1)+int(read2)+int(read3)+int(read4)+int(read5))/5
+            print("hypotenuse", hypotenuse_raw)
         except UnicodeDecodeError:
             print("unicode error! help!")
             pass
+        if hypotenuse_raw > 220 and hypotenuse_raw < 500:
+            '''change raw input to actual distance'''
+            new_polynomial = P([807.74 - float(hypotenuse_raw), -18.27, .137])      # defines polynomial from calibration
+            roots = new_polynomial.roots()                                          # finds roots of said polynomial
+            raw_distance = roots.item(0)                                            # takes the relevant one
+            print("roots", roots)
 
-        '''do the math'''
-        #TODO: insert equation to turn hypotenuse_raw into a distance
-        #TODO: insert equation to get real x and y Values
-        #TODO: append x and y values to lists
+            '''take polar coordinates and change to cartesian'''
+            angle_from_center = m.radians(int(new_angle)-90)
+            y_value = raw_distance*m.cos(angle_from_center)
+            x_value = -raw_distance*m.sin(angle_from_center)
+            # print("distance from sensor", y_value)
+            # print("distance from center", x_value)
+            xvalues.append(x_value)
+            yvalues.append(y_value)
+        else:
+            pass
+
+        i += 1
 
 zs = 0
 ax.scatter(xvalues, yvalues, zs)
---
+++ scanner2.ino/scanner2.ino.ino
@@ -0,0 +1,74 @@
+/*Arduino code that runs the 3D scan with 2 IR sensors
+ *Author: Hannah Kolano
+ *Last updated: 9/17/17
+ */
+
+#include <Servo.h>
+
+//set up variables and pins
+int ir_value = 0;      //variable for the IR reading
+int panpos = 0;        //variable for storing position of the pan servo
+String result = "";    //string to send to the computer
+const byte IR_PIN = A1; //pin of the IR input
+byte cmd_id = 0;
+int read1 = 0;
+int read2 = 0;
+int read3 = 0;
+int read4 = 0;
+int read5 = 0;
+
+//set up servos 
+Servo panservo;       //create servo objects
+
+
+void setup() {
+  panservo.attach(12);    //attaches servo on this pin to the servo object
+  pinMode(IR_PIN, INPUT);        //declares the IR as an input
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  int ir_value = analogRead(IR_PIN);
+
+  if(Serial.available() > 0) {
+    cmd_id = Serial.read();
+  } else {
+    cmd_id = 0;
+  }
+
+  switch(cmd_id) {
+    case 1:
+      scanPan();
+      returnPan();
+    default:
+      delay(15);
+  }
+}
+
+void scanPan() {
+  for (panpos = 45; panpos <= 135; panpos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    panservo.write(panpos);              // tell servo to go to position in variable 'pos'
+    delay(200);                       // waits 15ms for the servo to reach the position
+    read1 = analogRead(IR_PIN);
+    delay(100);
+    read2 = analogRead(IR_PIN);
+    delay(100);
+    read3 = analogRead(IR_PIN);
+    delay(100);
+    read4 = analogRead(IR_PIN);
+    delay(100);
+    read5 = analogRead(IR_PIN);
+    result = result + panpos + " " + read1 + " " + read2 + " " + read3 + " " + read4 + " " + read5;
+    Serial.println(result);
+    result = "";
+  }
+}
+
+void returnPan() {
+  for (panpos = 180; panpos >= 0; panpos -= 1) { // goes from 180 degrees to 0 degrees
+    panservo.write(panpos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
